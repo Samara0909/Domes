ed45741387759e5c138c7f61238c5cf4fade0c54
20221TIIMI0270 <soraiatosdeoliveira@hotmail.com>
2024-12-19 16:06:14 -0300

    Atualizações

--- app/src/main/java/soraia/samara/duda/victor/domes/activities/HistoricoActivity.java
+++
@@ -1,6 +0,0 @@
-package soraia.samara.duda.victor.domes.activities;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class HistoricoActivity extends AppCompatActivity {
-}
--- app/src/main/java/soraia/samara/duda/victor/domes/fragments/HistoricoFragment.java
+++ app/src/main/java/soraia/samara/duda/victor/domes/fragments/HistoricoFragment.java
@@ -61,6 +61,6 @@ public class HistoricoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historico, container, false);
+        return inflater.inflate(R.layout.item_historico, container, false);
     }
 }
